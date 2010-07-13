ea453a7076ca7e29b2cde606b3d2947ae4d31101
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-07-13 06:05:16 +0000

    Formalized the curation adding form.  Further tests added.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.annotation.curation/trunk@5189 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/annotation/curation/browser/form.py
+++ pmr2/annotation/curation/browser/form.py
@@ -1,18 +1,43 @@
 import zope.interface
 import zope.component
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory('pmr2.annotation.curation')
 
 from plone.z3cform import layout
 import z3c.form
 
+from pmr2.annotation.curation.schema.interfaces import *
 from pmr2.annotation.curation.interfaces import ICurationFlag
 from pmr2.annotation.curation.interfaces import ICurationTool
 from pmr2.annotation.curation.browser.interfaces import ICurationIdMixin
 from pmr2.annotation.curation.flag import CurationFlag
 
 
+class CurationToolDisplayForm(z3c.form.form.DisplayForm):
+    fields = z3c.form.field.Fields(ICurationTool)
+
+    template = ViewPageTemplateFile('manage_curation.pt')
+
+    def getContent(self):
+        return zope.component.getUtility(ICurationTool)
+
+    def flags(self):
+        tool = self.getContent()
+        flags = tool.listFlags()
+        keys = ['id', 'flag']
+        return [dict(zip(keys, flag)) for flag in flags.items()]
+
+    def __call__(self):
+        return self.render()
+
+CurationToolDisplayFormView = layout.wrap_form(CurationToolDisplayForm,
+    label = _(u'Curation Tool Management'))
+
+
 class CurationFlagAddForm(z3c.form.form.AddForm):
-    fields = z3c.form.field.Fields(ICurationFlag).omit('items') + \
-             z3c.form.field.Fields(ICurationIdMixin)
+    fields = z3c.form.field.Fields(ICurationIdMixin) + \
+             z3c.form.field.Fields(ICurationFlag).omit('items')
 
     def create(self, data):
         self.data = data
@@ -25,10 +50,13 @@ class CurationFlagAddForm(z3c.form.form.AddForm):
         tool = zope.component.getUtility(ICurationTool)
         name = self.data['id']
         if tool.getFlag(name):
-            # XXX currently we don't support overwriting builtin flags.
-            # XXX not implemented exception
+            # Currently we don't support overriding builtin flags.
             raise CurationFlagExistsError()
         tool.setFlag(name, obj)
 
     def nextURL(self):
-        pass
+        # assume context is portal root
+        return self.context.absolute_url() + '/@@manage_curation'
+
+CurationFlagAddFormView = layout.wrap_form(CurationFlagAddForm,
+    label = _(u'Add a Curation Flag'))
--- pmr2/annotation/curation/schema/interfaces.py
+++ pmr2/annotation/curation/schema/interfaces.py
@@ -1,6 +1,12 @@
 import zope.schema.interfaces
 
 
+class CurationFlagExistError(zope.schema.interfaces.ValidationError):
+    """\
+    Curation flag already exists.
+    """
+
+
 class ICurationDict(zope.schema.interfaces.IDict):
     """\
     Curation dictionary.
