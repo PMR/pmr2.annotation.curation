650faa037abd3aff8b448250c104976260fe68e1
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-07-29 12:36:56 +0000

    Added method to calculate values within the inputs and assign back to textarea.
      - Still needs to find a way to trigger this without too much overhead,
        either on edit or post.
    
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/pmr2.annotation.curation/trunk@5222 eb92df5b-a40c-0410-b02c-bf52feaaa166

--- pmr2/annotation/curation/browser/js/curationflagdictwidget.js
+++ pmr2/annotation/curation/browser/js/curationflagdictwidget.js
@@ -45,8 +45,27 @@ function appendNewWidgetRow(selector, widgetName)
   jq(selector + ' [name=' + widgetName + '-key' + row + ']').select();
 }
 
+function calcCurationFlagDictWidget(widgetId)
+{
+  // FIXME define this inside some sort of object to avoid repetition
+  var widgetName = 'curationflagdict';
+  var tag = 'table';
+  var divId = '#' + widgetName + '-' + widgetId;
+  var selector = divId + ' + ' + tag + ' > tbody input';
+
+  var inputs = jq(selector);
+  results = '';
+  for (var i = 0; i < inputs.length; i++) {
+    results = results + inputs[i].value + '\n';
+  }
+
+  jq('#' + widgetName).value = results;
+  alert(jq('#' + widgetName).value);
+}
+
 function appendCurationFlagDictWidget(widgetId)
 {
+  // FIXME define this inside some sort of object to avoid repetition
   var widgetName = 'curationflagdict';
   var tag = 'table';
   var divId = '#' + widgetName + '-' + widgetId;
@@ -60,13 +79,13 @@ function appendCurationFlagDictWidget(widgetId)
   values.reverse();
 
   var structure = jq(divId + ' + ' + tag);
-  structure.append('<thead><tr><th>Flag Values</th><th>Description</th></tr></thead>');
+  structure.append('<thead><tr><th>Flag Values</th>' +
+                   '<th>Description</th></tr></thead>');
   structure.append('<tbody></tbody>');
 
   var selector = divId + ' + ' + tag + ' > tbody';
 
-  while (values.length)
-  {
+  while (values.length) {
     key = values.pop();
     value = values.pop();
     i = values.length;
@@ -76,4 +95,7 @@ function appendCurationFlagDictWidget(widgetId)
     '<tfoot><tr><th colspan="2"><button type="button" value="New Flag" ' +
     'onclick="javascript:appendNewWidgetRow(\'' + selector + '\', \'' + 
     widgetName + '\')">New Flag</button></th></tr>');
+  jq(textareaId).parents('form').bind(
+    'submit', function f() {calcCurationFlagDictWidget(widgetId)});
 }
+
