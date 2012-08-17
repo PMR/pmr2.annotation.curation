a2d835e405d13a45ee5c88b2c373f0f7c447bec7
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-08-17 18:32:15 +1200

    Correcting the basic display and tests.
      - Temporary workaround for missing flags.
      - Will need to formalize handling of undefined (or removed) flags
        later.

--- pmr2/annotation/curation/browser/view.py
+++ pmr2/annotation/curation/browser/view.py
@@ -21,6 +21,9 @@ class BasicCurationNote(ExposureFileViewBase):
         curation = zope.component.getUtility(ICurationTool)
         for k, v in self.note.flags.iteritems():
             key = curation.getFlag(k)
+            # XXX this can be None, workaround
+            if key is None:
+                key = type('DummyFlag', (object,), {'title': k})
             value = v and v[0] or '0'
             yield {
                 'key': key,
