dfc09de55f5d31f21455d6a8298e610d599a1256
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-11-06 18:32:28 +1300

    Make the scrubbing more generalized.
    Progress towards: tracker item 3461

--- pmr2/annotation/curation/annotator.py
+++ pmr2/annotation/curation/annotator.py
@@ -9,6 +9,8 @@ from pmr2.app.annotation.annotator import ExposureFileAnnotatorBase
 
 from pmr2.annotation.curation.interfaces import *
 
+from pmr2.annotation.curation.util import scrub_json_unicode_to_string
+
 
 class BasicCurationAnnotator(ExposureFileAnnotatorBase):
     """\
@@ -25,5 +27,24 @@ class BasicCurationAnnotator(ExposureFileAnnotatorBase):
     label = u'Model Curation'
     description = u'Basic curation flags assigned to this item.'
     for_interface = IBasicCurationSet
+
+    def _annotate(self, data):
+        # XXX wholesale reimplementation of core method.  Once a proper
+        # filter is implemented by parent class we can avoid doing this.
+        note = self.note
+        try:
+            for a, v in data:
+                # XXX figure out how to gracefully handle schema errors
+                # (such as missing values).
+                if a == 'flags':
+                    v = scrub_json_unicode_to_string(v)
+                setattr(note, a, v)
+        except TypeError:
+            raise TypeError('%s.generate failed to return a list of ' \
+                            'tuple(key, value)' % self.__class__)
+        except ValueError:
+            raise ValueError('%s.generate returned invalid values (not ' \
+                             'list of tuple(key, value)' % self.__class__)
+
 
 BasicCurationAnnotatorFactory = named_factory(BasicCurationAnnotator)
