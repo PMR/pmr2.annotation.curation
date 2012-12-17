4683ce5a3bc0c33b813bd58046113f72e205afac
Tommy Yu <tommy.yu@auckland.ac.nz>
2012-12-17 19:15:27 +1300

    Correcting references to portal_url
      - These still need to be corrected to follow more standardized usage.

--- pmr2/annotation/curation/browser/view.py
+++ pmr2/annotation/curation/browser/view.py
@@ -1,6 +1,8 @@
 import zope.component
 from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 
+from Products.CMFCore.utils import getToolByName
+
 from pmr2.app.exposure.browser.browser import ExposureFileViewBase
 
 from pmr2.annotation.curation.interfaces import ICurationTool
@@ -13,6 +15,10 @@ class BasicCurationNote(ExposureFileViewBase):
     """
 
     template = ViewPageTemplateFile(path('basic_curation_note.pt'))
+
+    def portal_url(self):
+        portal = getToolByName(self.context, 'portal_url').getPortalObject()
+        return portal.absolute_url()
 
     def items(self):
         curation = zope.component.getUtility(ICurationTool)
